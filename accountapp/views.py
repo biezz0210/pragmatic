db2c6e6a6528e476114e75cb80028938174246ac
biezz0210 <biezz0210@gmail.com>
2020-12-29 21:36:26 +0900

    django course 21 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -20,3 +23,11 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # reverse는 def, reverse_lazy는 class에서 사용
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
