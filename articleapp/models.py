cf789072efc637f93f62ba4296a31386413c6517
biezz0210 <biezz0210@gmail.com>
2021-01-11 21:41:55 +0900

    fix article model image field

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from projectapp.models import Project
+from uuid import uuid4
+
+
+def get_file_path(instance, filename):
+    uuid_name = uuid4().hex
+    return '/'.join(['article/', uuid_name])
+
 
 # Create your models here.
 
@@ -13,7 +20,8 @@ class Article(models.Model):
         Project, on_delete=models.SET_NULL, related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
-    image = models.ImageField(upload_to='article/', null=False)
+    image = models.ImageField(upload_to=get_file_path, null=False)
+    # image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
 
     created_at = models.DateField(auto_created=True, null=True)
