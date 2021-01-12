bfa706892f20808db09e12cb1968764af4d3e3e5
biezz0210 <biezz0210@gmail.com>
2021-01-12 11:30:54 +0900

    delete get_file_path

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from projectapp.models import Project
-# from uuid import uuid4
-
-
-# def get_file_path(instance, filename):
-#     uuid_name = uuid4().hex
-#     return '/'.join(['article/', uuid_name])
-
-
-# Create your models here.
 
 
 class Article(models.Model):
@@ -20,7 +11,6 @@ class Article(models.Model):
         Project, on_delete=models.SET_NULL, related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
-    # image = models.ImageField(upload_to=get_file_path, null=False)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
 
